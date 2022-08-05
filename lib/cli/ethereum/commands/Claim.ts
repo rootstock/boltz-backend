66507bbd37d016126469a164b6dc1504b472dabd
Patricio <patricio@iovlabs.org>
2022-08-05 11:14:30 -0600

    feature: Add complete swap flow integration test.

--- lib/cli/ethereum/commands/Claim.ts
+++ lib/cli/ethereum/commands/Claim.ts
@@ -21,6 +21,7 @@ export const builder = {
 export const handler = async (argv: Arguments<any>): Promise<ContractReceipt> => {
 
   const signer = connectEthereum(argv.provider);
+  console.log('connectEthereum success.')
   const { etherSwap, erc20Swap } = await getContracts(signer);
 
   const preimage = getHexBuffer(argv.preimage);
@@ -37,7 +38,9 @@ export const handler = async (argv: Arguments<any>): Promise<ContractReceipt> =>
       erc20SwapValues.timelock,
     );
   } else {
+    console.log('queryEtherSwapValues...')
     const etherSwapValues = await queryEtherSwapValues(etherSwap, crypto.sha256(preimage));
+    console.log('etherSwap.claim...')
     transaction = await etherSwap.claim(
       preimage,
       etherSwapValues.amount,
